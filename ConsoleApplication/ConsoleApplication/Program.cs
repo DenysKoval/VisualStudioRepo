78059d6089512af9c5c1f37ab203185d02dd9df6
Denys Koval <denys_koval@epam.com>
2017-03-20 16:02:18 +0200

    firstPushWithStudio

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integer = new List<int>();
+            integer.Add(1);
+            integer.Add(2);
+            integer.Add(3);
+            for (int i = 0; i<integer.Count;i++)
+                {
+                    Console.WriteLine(integer[i]);
+                }
+        }
+    }
+}
